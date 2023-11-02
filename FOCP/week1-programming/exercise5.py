b4950766a622d35a6bdc05b51cdd2f545af64fce
swornims <iamswornimshakya@gmail.com>
2023-11-02 12:03:19 +0545

    Changes to exercie5

--- FOCP/week1-programming/exercise5.py
+++ FOCP/week1-programming/exercise5.py
@@ -1,19 +1,23 @@
 # Exercise 5
 
-def sum(arr):
-    if len(arr) == 1:
-        return arr[0]
-    else:
-        return arr[0] + sum(arr[1:])
+# def sum(arr):
+#     if len(arr) == 1:
+#         return arr[0]
+#     else:
+#         return arr[0] + sum(arr[1:])
 
 
-arr = [113, 175, 12]
-
-total_students = sum(arr)
-
+class_size = [113, 175, 12]
 lab_group = 24
 
-complete_groups, remaining_students = divmod(total_students, lab_group)
+for student in class_size:
+    complete_groups = student // lab_group
+    remaining_students = student % lab_group
 
-print('Total number of complete groups', complete_groups)
-print('Remaining students', remaining_students)
+    print('Group size:', student)
+    print('Total number of complete groups:', complete_groups)
+    print('Remaining students:', remaining_students)
+
+
+# total_students = sum(arr)
+# complete_groups, remaining_students = divmod(total_students, lab_group)
